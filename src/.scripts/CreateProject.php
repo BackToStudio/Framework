2e1daa0825cc4a045bf791c8165ea7a31f84556d
Jonathan Alpay <jonathan@fantassin.fr>
2019-10-03 18:50:57 +0200

    Copy default folder into the project

---
+++ src/.scripts/CreateProject.php
@@ -0,0 +1,31 @@
+	<?php
+	namespace Bazooka;
+
+	class CreateProject {
+
+	public static function init() {
+		echo "\n\n";
+		echo "🧠 Installing Bazooka Core \n\n";	
+
+		CreateProject::moveFiles("default/", "../");
+	}
+
+	public static function moveFiles(string $source, string $destination) {
+		// Open a known sourceectory, and proceed to read its contents
+		if (is_source($source)) {
+			if ($stream = opensource($source)) {
+				while (($file = readsource($stream)) !== false) {
+					//exclude unwanted 
+					if ($file==".") continue;
+					if ($file=="..")continue;
+					//if ($file=="index.php") continue; for example if you have index.php in the folder
+					if (rename($source.'/'.$file,$destination.'/'.$file)) {
+						echo "Copying files... \n";
+					}
+				}
+
+				closesource($stream);
+			}
+		}
+	}
+}
