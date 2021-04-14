3efc84752c3eaa749c227ab79525e03290b30022
Florian Truchot <florian@fantassin.fr>
2021-04-14 11:48:27 +0200

    test: Add PostTypeRegistry unit tests

---
+++ src/PostType/Tests/PostTypeRegistryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Fantassin\Core\WordPress\PostType\Tests;
+
+use Fantassin\Core\WordPress\PostType\PostType;
+use Fantassin\Core\WordPress\PostType\PostTypeInterface;
+use Fantassin\Core\WordPress\PostType\PostTypeRegistry;
+use PHPUnit\Framework\TestCase;
+
+class PostTypeA implements PostTypeInterface{
+
+    public function getKey(): string
+    {
+        return 'a';
+    }
+
+    public function getArgs(): array
+    {
+        return [];
+    }
+}
+
+class PostTypeB implements PostTypeInterface{
+
+    public function getKey(): string
+    {
+        return 'b';
+    }
+
+    public function getArgs(): array
+    {
+        return [];
+    }
+}
+
+class PostTypeRegistryTest extends TestCase
+{
+
+    public function testEmptyRegistry()
+    {
+        $registry = new PostTypeRegistry();
+        $this->assertCount(0, $registry->getPostTypes());
+    }
+
+    public function testAddingPostType()
+    {
+        $registry = new PostTypeRegistry();
+        $postTypeA = new PostTypeA();
+        $postTypeB = new PostTypeB();
+        $registry->add($postTypeA);
+        $registry->add($postTypeB);
+        $this->assertCount(2, $registry->getPostTypes());
+    }
+}
