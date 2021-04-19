6125ad040c18914fa1c0006f145d011646cfffca
Florian Truchot <florian@fantassin.fr>
2021-04-19 15:37:35 +0200

    test: add Taxonomy Registry unit tests

---
+++ src/Taxonomy/Tests/TaxonomyRegistryTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Fantassin\Core\WordPress\Taxonomy\Tests;
+
+use Fantassin\Core\WordPress\Contracts\RegistryInterface;
+use Fantassin\Core\WordPress\Taxonomy\TaxonomyInterface;
+use Fantassin\Core\WordPress\Taxonomy\TaxonomyRegistry;
+use PHPUnit\Framework\TestCase;
+
+class TaxonomyA implements TaxonomyInterface
+{
+
+    public function getKey(): string
+    {
+        return 'a';
+    }
+
+    public function getArgs(): array
+    {
+        return [];
+    }
+
+    public function getPostTypes(): array
+    {
+        return [];
+    }
+}
+
+class TaxonomyB implements TaxonomyInterface
+{
+
+    public function getKey(): string
+    {
+        return 'b';
+    }
+
+    public function getArgs(): array
+    {
+        return [];
+    }
+
+    public function getPostTypes(): array
+    {
+        return [ 'abcde' ];
+    }
+}
+
+class TaxonomyRegistryTest extends TestCase
+{
+
+    public function testTaxonomyRegistryShouldImplementsRegistryInterface()
+    {
+        $registry = new TaxonomyRegistry();
+        $this->assertInstanceOf(RegistryInterface::class, $registry);
+    }
+
+    public function testEmptyRegistry()
+    {
+        $registry = new TaxonomyRegistry();
+        $this->assertCount(0, $registry->getTaxonomies());
+    }
+
+    public function testAddingTaxonomy()
+    {
+        $registry = new TaxonomyRegistry();
+        $taxonomyA = new TaxonomyA();
+        $taxonomyB = new TaxonomyB();
+        $registry->add($taxonomyA);
+        $registry->add($taxonomyB);
+        $this->assertCount(2, $registry->getTaxonomies());
+        $this->assertContains($taxonomyA, $registry->getTaxonomies());
+        $this->assertContains($taxonomyB, $registry->getTaxonomies());
+    }
+}
