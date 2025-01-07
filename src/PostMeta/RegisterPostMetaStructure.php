1f05299f19dcb4fc5edb992313b9e0e26431bae6
Suriteka <me@suriteka.com>
2025-01-07 11:18:23 +0100

    fix: update error of variable

--- src/PostMeta/RegisterPostMetaStructure.php
+++ src/PostMeta/RegisterPostMetaStructure.php
@@ -22,29 +22,29 @@ class RegisterPostMetaStructure implements Hooks {
 
     public function registerPostMeta(): void
     {
-        $args = [
-            'object_subtype' => $postMetaStructure->getObjectSubtype(),
-            'type' => $postMetaStructure->getType(),
-            'label' => $postMetaStructure->getLabel(),
-            'description' => $postMetaStructure->getDescription(),
-            'single' => $postMetaStructure->isSingle(),
-            'show_in_rest' => $postMetaStructure->isShowInRest(),
-            'revisions_enabled' => $postMetaStructure->isRevisionsEnabled(),
-        ];
-
-        if (!is_null($postMetaStructure->getDefault())) {
-            $args['default'] = $postMetaStructure->getDefault();
-        }
-
-        if (is_callable($postMetaStructure->getSanitizeCallback())) {
-            $args['sanitize_callback'] = $postMetaStructure->getSanitizeCallback();
-        }
-
-        if (is_callable($postMetaStructure->getAuthCallback())) {
-            $args['auth_callback'] = $postMetaStructure->getAuthCallback();
-        }   
-
         foreach ($this->registry->getPostMetaStructures() as $postMetaStructure) {
+            $args = [
+                'object_subtype' => $postMetaStructure->getObjectSubtype(),
+                'type' => $postMetaStructure->getType(),
+                'label' => $postMetaStructure->getLabel(),
+                'description' => $postMetaStructure->getDescription(),
+                'single' => $postMetaStructure->isSingle(),
+                'show_in_rest' => $postMetaStructure->isShowInRest(),
+                'revisions_enabled' => $postMetaStructure->isRevisionsEnabled(),
+            ];
+    
+            if (!is_null($postMetaStructure->getDefault())) {
+                $args['default'] = $postMetaStructure->getDefault();
+            }
+    
+            if (is_callable($postMetaStructure->getSanitizeCallback())) {
+                $args['sanitize_callback'] = $postMetaStructure->getSanitizeCallback();
+            }
+    
+            if (is_callable($postMetaStructure->getAuthCallback())) {
+                $args['auth_callback'] = $postMetaStructure->getAuthCallback();
+            }   
+
             register_post_meta($postMetaStructure->getObjectType(), $postMetaStructure->getMetaKey(), $args);
         }
     }
