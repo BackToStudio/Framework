874e4881fee16172b167402f2d4fc335edcfae68
Suriteka <me@suriteka.com>
2025-01-07 10:54:01 +0100

    fix: compilerPass

--- src/PostMeta/DependencyInjection/Compiler/RegisterPostMetaStructurePass.php
+++ src/PostMeta/DependencyInjection/Compiler/RegisterPostMetaStructurePass.php
@@ -2,7 +2,7 @@
 
 namespace BackTo\Framework\PostMeta\DependencyInjection\Compiler;
 
-use BackTo\Framework\PostMeta\RegisterPostMetaStructure;
+use BackTo\Framework\PostMeta\PostMetaStructureRegistry;
 use BackToVendor\Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use BackToVendor\Symfony\Component\DependencyInjection\ContainerBuilder;
 use BackToVendor\Symfony\Component\DependencyInjection\Reference;
@@ -15,11 +15,11 @@ class RegisterPostMetaStructurePass implements CompilerPassInterface
 
     public function process(ContainerBuilder $container)
     {
-        if (!$container->hasDefinition(RegisterPostMetaStructure::class)) {
+        if (!$container->hasDefinition(PostMetaStructureRegistry::class)) {
             return;
         }
 
-        $registryDefinition = $container->findDefinition(RegisterPostMetaStructure::class);
+        $registryDefinition = $container->findDefinition(PostMetaStructureRegistry::class);
 
         foreach ($container->findTaggedServiceIds('wordpress.post_meta') as $id => $tags) {
             $registryDefinition->addMethodCall('add', [new Reference($id)]);
